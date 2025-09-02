0f382998fe786b0f4e6f45a9999363775f2bc58c
d3lac0baa <sercoga2006@gmail.com>
2025-09-03 00:54:09 +0200

    upload

---
+++ basicos22_java.js
@@ -0,0 +1,24 @@
+const fruits = ["Strawberry", "Banana", "Orange", "Apple"];
+
+const foodSchedule = [
+  { name: "Heura", isVegan: true },
+  { name: "Salmon", isVegan: false },
+  { name: "Tofu", isVegan: true },
+  { name: "Burger", isVegan: false },
+  { name: "Rice", isVegan: true },
+  { name: "Pasta", isVegan: true },
+];
+
+let fruitIndex = 0;
+
+for (let i = 0; i < foodSchedule.length; i++) {
+  if (!foodSchedule[i].isVegan) {
+    if (fruitIndex < fruits.length) {
+      foodSchedule[i].name = fruits[fruitIndex];
+      foodSchedule[i].isVegan = true;
+      fruitIndex++;
+    }
+  }
+}
+
+console.log(foodSchedule);
